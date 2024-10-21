dc4980a5a29aff49d0600c105a240d4642db8c6b
iiiitsu <ne_in@foxmail.com>
2024-10-20 20:51:36 -0700

    integrated end2end

---
+++ prompts/slide_prompts.py
@@ -0,0 +1,11 @@
+def get_each_page_prompt():
+    EACH_PAGE_PROMPT_PATH = './prompts/each_page_prompt' # Path to the prompt file
+    with open(EACH_PAGE_PROMPT_PATH, 'r') as file:
+        each_page_prompt = file.read()
+    return each_page_prompt
+        
+def get_summarizing_prompt():
+    SUMMARIZING_PROMPT_PATH = './prompts/summarizing_prompt' # Path to the prompt file
+    with open(SUMMARIZING_PROMPT_PATH, 'r') as file:
+        summarizing_prompt = file.read()
+    return summarizing_prompt
