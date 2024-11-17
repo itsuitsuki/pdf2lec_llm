50d310b8661fbfa8c1b360de6dafe649b60c8064
ORI <hongmt2022@shanghaitech.edu.cn>
2024-11-17 14:04:50 -0800

    refactor: restructure project layout
    - Remove files from old structure
    - Add redis configuration directory
    - Reorganize project structure for better maintainability

--- test_splitting_api.sh
+++
@@ -1,7 +0,0 @@
-curl -X POST http://localhost:5000/api/v1/split_merge_pdf \
--H "Content-Type: application/json" \
--d '{
-    "pdf_name": "cs168-fa24-lec14",
-    "similarity_threshold": 0.4,
-    "debug_mode": false
-}'
